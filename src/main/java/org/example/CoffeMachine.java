1fae5039fc2954322ada00e0c6a4b40e79fdc63e
eevgen <evgeniy.kalenichenko@gmail.com>
2023-07-03 14:11:34 +0300

    Add doingCoffee() which is doing for()

--- src/main/java/org/example/CoffeMachine.java
+++ src/main/java/org/example/CoffeMachine.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.util.Random;
+
 public class CoffeMachine {
 
     public void doingCoffee() {
-        for (int i = 0; i < ; i++) {
-            
+        for (int i = 0; i < (new Random()).nextInt(100, 1000); i++) {
+            System.out.println("...Doing coffee...");
+            System.out.println("...Showing loading process...");
         }
     }
 
